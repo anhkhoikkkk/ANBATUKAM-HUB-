2e27c3ed94a31a6e7d6e02f3d2abadfff8ed434d
anhkhoikkkk <duonganhkhoizzz@gmail.com>
2025-01-23 09:39:17 +0700

    Add files via upload

---
+++ khoiroblox.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "AK HUB",--Tên Script
+    logoscript = "71666162816749",--ID Logo Script
+    tacgia = "sadas"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
